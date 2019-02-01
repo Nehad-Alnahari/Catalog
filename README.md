9ae550be8f03b43208d5352cf802922424aa359d
Nehad-Alnahari <miss_naddooo@hotmail.com>
2019-02-01 18:11:53 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,29 @@
+**INTRODUCTION**
+This project aims to create a website using your own database with some tools 
+(sqlalchemy, HTML templates, css decoratores, vertual box, python, and vagrant software). 
+
+
+**TOOLS TO RUN THE WEBSITE** 
+you nedd to install some tools:
+1. VM on your computer 
+2. Python 2
+3. vagrant software 
+4. VM configuration which you can download it and unzip it : FSND-Virtual-Machine.zip
+5. Download or clone my project
+
+
+**ISTRUCTIONS TO RUN THE PROJECT** 
+1. Use your own termenal
+2. Hit cd to go to the directory that contains the project
+3. Hit cd again to the vagrant file
+4. follow the commands bellow:
+    # vagrant up
+    # vagrant ssh
+    # cd /vagrant
+5. Now run the database and the website by following the bellow commands
+    # python database_setup.py
+    # python database_fill.py
+    # python games_project.py
+6. Now go to yoyr browser and write this path: localhos:8000, and now you are in the wibsite 
+7. You can close the website fron the terminal using control and C.
+
---
+++ database_setup.py
@@ -0,0 +1,71 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
+
+class Category(Base):
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'id': self.id
+        }
+
+
+class Game(Base):
+    __tablename__ = 'games'
+
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    description = Column(String(3000))
+    picture = Column(String(250))
+    platform = Column(String(250))
+    developer = Column(String(250))
+    publisher = Column(String(250))
+    producer = Column(String(250))
+    director = Column(String(250))
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+            'picture': self.picture,
+            'platform': self.platform,
+            'developer': self.developer,
+            'publisher':self.publisher,
+            'producer': self.producer,
+            'director': self.director,
+        }
+
+
+engine = create_engine('sqlite:///Games.db')
+
+
+Base.metadata.create_all(engine)
